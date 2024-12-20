32e9d4eca8587e2aeda3a46ad11cd38b6ade481d
hackerx20 <shivansh_t@mt.iitr.ac.in>
2024-12-20 15:54:35 +0530

    Started Tracking Sample_Program.cpp again

---
+++ Sample_Program.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Learning Git and Github"<<endl;
+    cout<<"Added this file to .gitignore"<<endl;
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
