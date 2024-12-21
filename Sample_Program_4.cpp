de33955f2a59966052907587413ff73617462c62
hackerx20 <shivansh_t@mt.iitr.ac.in>
2024-12-21 08:55:46 +0530

    Learning Merge and file deletion

---
+++ Sample_Program_4.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+    cout<<"Go to Hell"<<endl;
+    return 0;
+}
